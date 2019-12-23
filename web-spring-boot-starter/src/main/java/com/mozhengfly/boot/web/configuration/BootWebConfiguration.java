c41ecd1f512cbb12584060645e43741434ddd91e
wangchonglin <wangchonglin@thunisoft.com>
2019-12-23 14:58:56 +0800

    web starter 自动装配类

---
+++ web-spring-boot-starter/src/main/java/com/mozhengfly/boot/web/configuration/BootWebConfiguration.java
@@ -0,0 +1,16 @@
+package com.mozhengfly.boot.web.configuration;
+
+import com.mozhengfly.boot.web.annotation.EnableApiVersion;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * BootWebConfiguration
+ * @Description BootWebStarter配置类
+ * @Author mozhengfly
+ * @Date 2019-12-23 14:52:16
+ * @Version 1.0.0
+ */
+@EnableApiVersion
+@Configuration
+public class BootWebConfiguration {
+}
