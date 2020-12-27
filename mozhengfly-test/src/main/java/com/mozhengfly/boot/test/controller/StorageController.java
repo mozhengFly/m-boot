6732d098f063cd0c2916f2b3f9f878604468d0f6
mozhengfly <458142816@qq.com>
2020-12-27 21:02:23 +0800

    增加邮件配置

--- mozhengfly-test/src/main/java/com/mozhengfly/boot/test/controller/StorageController.java
+++ mozhengfly-test/src/main/java/com/mozhengfly/boot/test/controller/StorageController.java
@@ -2,6 +2,7 @@ package com.mozhengfly.boot.test.controller;
 
 import com.mozhengfly.boot.test.vo.User;
 import com.mozhengfly.boot.web.annotation.ApiVersion;
+import com.mozhengfly.boot.web.rate.RateLimit;
 import io.swagger.annotations.Api;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.PatternMatchUtils;
@@ -25,8 +26,14 @@ public class StorageController {
         return ResponseEntity.ok("hello " + user.getName());
     }
 
+    @RateLimit(0.5)
     @GetMapping("hi")
     public ResponseEntity hi() {
+        return ResponseEntity.ok("mozhengfly");
+    }
+
+    @GetMapping("hi2")
+    public ResponseEntity hi2() {
         return ResponseEntity.ok("mozhengfly");
     }
 
